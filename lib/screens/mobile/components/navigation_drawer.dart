5497b8b411f95a09bdb7694dd200f62bd4263b79
Joel Odufu <joel.odufu@gmail.com>
2023-05-04 20:40:15 +0100

    Added Drawer

--- lib/screens/mobile/components/navigation_drawer.dart
+++ lib/screens/mobile/components/navigation_drawer.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:kuziem/constants.dart';
-import 'package:kuziem/screens/mobile/courses/courses_screen.dart';
-import 'package:kuziem/screens/mobile/student_home/components/body.dart';
 
-class StudentHomeScreen extends StatelessWidget {
-  static String routeName = "/student_home";
-  const StudentHomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Body(),
-      drawer: NavigationDrawer(),
-    );
-  }
-}
+import '../courses/courses_screen.dart';
+import '../student_home/student_home_screen.dart';
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({super.key});
@@ -33,36 +17,7 @@ class NavigationDrawer extends StatelessWidget {
 }
 
 Widget buildHeader(BuildContext context) => Container(
-      color: kPrimaryColor,
       padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-      child: InkWell(
-        onTap: () {},
-        child: Container(
-          color: kPrimaryColor,
-          padding: EdgeInsets.only(
-              top: 24 + MediaQuery.of(context).padding.top, bottom: 24),
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 52,
-                backgroundImage: NetworkImage(
-                    "https://www.facebook.com/photo/?fbid=5140395726062009&set=a.255499527885011"),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                "Joel Odufu",
-                style: TextStyle(fontSize: 28, color: Colors.white),
-              ),
-              Text(
-                "joel.odufu@gmail.com",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
 
 Widget buildMenuItems(BuildContext context) => Container(
